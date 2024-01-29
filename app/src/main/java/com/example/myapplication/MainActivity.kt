acf282bb48af0f9ae407a4ec51477e7b6213e4af
mmiie0 <jixxxx@naver.com>
2024-01-29 21:35:57 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+//파일의 위치
+package com.example.myapplication
+//외부 패키지 클래스 임포트
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+//클래스 생성
+class MainActivity : AppCompatActivity() {
+    //변수 생성
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        }
+
+
+    }
+}
